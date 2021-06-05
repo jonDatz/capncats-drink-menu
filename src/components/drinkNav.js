2c44e6411751725eed51c8d733017f08980bd2e9
jonDatz <notjondatz@gmail.com>
2021-06-05 19:24:57 -0400

    Adding Manual Drink List
    Fix this to use json or contentful in the future

--- src/components/drinkNav.js
+++ src/components/drinkNav.js
@@ -3,10 +3,12 @@ import PropTypes from "prop-types"
 import SwipeableViews from "react-swipeable-views"
 import Typography from "@material-ui/core/Typography"
 import Box from "@material-ui/core/Box"
-import { makeStyles, useTheme, withStyles } from "@material-ui/core/styles"
+import { makeStyles, useTheme } from "@material-ui/core/styles"
 import AppBar from "@material-ui/core/AppBar"
 import Tabs from "@material-ui/core/Tabs"
 import Tab from "@material-ui/core/Tab"
+import Paper from "@material-ui/core/Paper"
+import Grid from "@material-ui/core/Grid"
 
 function TabbedPanel(props) {
   const { children, value, index, ...other } = props
@@ -44,17 +46,17 @@ function a11yProps(index) {
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,},
-  highlights:{
+    backgroundColor: theme.palette.background.paper,
+  },
+  highlights: {
     backgroundColor: "#2f2f2f",
-    inkBarStyle: '#e8bd51'
-  }
-// This changes the text under the bar
-
-  }
-
-))
-
+    inkBarStyle: "#e8bd51",
+  },
+  drinklist: {
+    fontFamily: "Chelsea Market",
+  },
+  // This changes the text under the bar
+}))
 
 export default function FullWidthTabs() {
   const classes = useStyles()
@@ -71,9 +73,10 @@ export default function FullWidthTabs() {
   return (
     <div className={classes.root}>
       <AppBar className={classes.highlights} position="static">
-        <Tabs TabIndicatorProps={{
-           style: { background: '#e8bd51'} //this changed the tabindicator color.
-         }}
+        <Tabs
+          TabIndicatorProps={{
+            style: { background: "#e8bd51" }, //this changed the tabindicator color.
+          }}
           value={value}
           onChange={handleChange}
           aria-label="header"
@@ -91,16 +94,65 @@ export default function FullWidthTabs() {
         onChangeIndex={handleChangeIndex}
       >
         <TabbedPanel value={value} index={0}>
-          Item One
+          <h2 className={classes.drinklist}>Miller Lite</h2>
+          <h2 className={classes.drinklist}>Coors Light</h2>
+          <h2 className={classes.drinklist}>Bud light</h2>
+          <h2 className={classes.drinklist}>Corona</h2>
+          <h2 className={classes.drinklist}>Corona Light</h2>
+          <h2 className={classes.drinklist}>Michelob Ultra</h2>
+          <h2 className={classes.drinklist}>Miller High Life</h2>
+          <h2 className={classes.drinklist}>Yuengling</h2>
+          <h2 className={classes.drinklist}>Heineken</h2>
+          <h2 className={classes.drinklist}>Guinness</h2>
+          <h2 className={classes.drinklist}>Twisted Tea</h2>
+          <h2 className={classes.drinklist}>
+            Bud Light Seltzer (Cherry/Mango)
+          </h2>
+          <h2 className={classes.drinklist}>White Claw (Cherry/Mango)</h2>
         </TabbedPanel>
+
         <TabbedPanel value={value} index={1}>
-          Item Two
+          <h2 className={classes.drinklist}>Leinenkugel Summer Shandy</h2>
+          <h2 className={classes.drinklist}>Bonesaw Squeezins</h2>
+          <h2 className={classes.drinklist}>Bonesaw Swoosh</h2>
+          <h2 className={classes.drinklist}>Sam Adams Summer Ale</h2>
+          <h2 className={classes.drinklist}>Miller Lite</h2>
+          <h2 className={classes.drinklist}>Bud Light</h2>
+          <h2 className={classes.drinklist}>Budweiser</h2>
+          <h2 className={classes.drinklist}>Yuengling</h2>
+          <h2 className={classes.drinklist}>Stella</h2>
+          <h2 className={classes.drinklist}>Blue Moon</h2>
         </TabbedPanel>
+
         <TabbedPanel value={value} index={2}>
-          Item Three
+          <h2 className={classes.drinklist}>Margarita</h2>
+          <h2 className={classes.drinklist}>Bloody Mary</h2>
+          <h2 className={classes.drinklist}>Martinis</h2>
+          <h2 className={classes.drinklist}>Cosmopolitan</h2>
+          <h2 className={classes.drinklist}>Manhattan</h2>
+          <h2 className={classes.drinklist}>Tequila Sunrise</h2>
+          <h2 className={classes.drinklist}>Woo Woo</h2>
+          <h2 className={classes.drinklist}>Madori Sour</h2>
+          <h2 className={classes.drinklist}>Moscow Mule</h2>
+          <h2 className={classes.drinklist}>Paloma</h2>
+          <h2 className={classes.drinklist}>June Bug</h2>
+          <h2 className={classes.drinklist}>Long Island</h2>
+          <h2 className={classes.drinklist}>Bay Breeze</h2>
+          <h2 className={classes.drinklist}>White Russian</h2>
+          <h2 className={classes.drinklist}>Whiskey Sour</h2>
+          <h2 className={classes.drinklist}>Old Fashioned</h2>
+          <h2 className={classes.drinklist}>007</h2>
+          <h2 className={classes.drinklist}>Jolly Rancher</h2>
         </TabbedPanel>
+
         <TabbedPanel value={value} index={3}>
-          Item 4
+          <h2 className={classes.drinklist}>Forestville Chardonnay</h2>
+          <h2 className={classes.drinklist}>Forestville White Zinfandel</h2>
+          <h2 className={classes.drinklist}>Belmondo Pinot Grigio</h2>
+          <h2 className={classes.drinklist}>Rex Goliath Moscato</h2>
+          <h2 className={classes.drinklist}>Forestville Merlot</h2>
+          <h2 className={classes.drinklist}>Forestville Cabernet</h2>
+          <h2 className={classes.drinklist}>Rex Goliath Pinot Noir</h2>
         </TabbedPanel>
       </SwipeableViews>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,12 @@ import DrinkNavigation from "../components/drinkNav"
 
 
 
+
 const IndexPage = () => (
 
   <Layout>
     <Seo title="Home" />
     <DrinkNavigation />
-    <p></p>
-
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
